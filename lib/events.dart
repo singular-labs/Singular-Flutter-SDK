eab006972bb43dfca5c78e69738da7114d965ea8
jzviaginSingular <100220215+jzviaginSingular@users.noreply.github.com>
2022-06-14 17:17:02 +0300

    added structured events

---
+++ lib/events.dart
@@ -0,0 +1,16 @@
+class Events{
+	static String get sngRate => "sng_rate";
+	static String get sngSpentCredits => "sng_spent_credits";
+	static String get sngTutorialComplete => "sng_tutorial_complete";
+	static String get sngLogin => "sng_login";
+	static String get sngStartTrial => "sng_start_trial";
+	static String get sngSubscribe => "sng_subscribe";
+	static String get sngBook => "sng_book";
+	static String get sngContentViewList => "sng_content_view_list";
+	static String get sngInvite => "sng_invite";
+	static String get sngShare => "sng_share";
+	static String get sngSubmitApplication => "sng_submit_application";
+	static String get sngUpdate => "sng_update";
+	static String get sngEcommercePurchase => "sng_ecommerce_purchase";
+	static String get sngViewCart => "sng_view_cart";
+}
